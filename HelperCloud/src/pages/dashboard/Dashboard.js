47467a414b78fa0bd356810ee392cb62406d768a
Jaten <33936505+JavierTen@users.noreply.github.com>
2019-10-13 05:32:53 -0500

    Update Inicio
    Arreglando la cagada de hace unos minutos donde entre Jasar y yo con un commit nos cagamos lo que habiamos hecho

--- HelperCloud/src/pages/dashboard/Dashboard.js
+++ HelperCloud/src/pages/dashboard/Dashboard.js
@@ -1,109 +1,103 @@
-import React, { useState } from "react";
-import {
-  Grid,
-  LinearProgress,
-  Select,
-  OutlinedInput,
-  MenuItem,
-  Button,
-} from "@material-ui/core";
-import { useTheme } from "@material-ui/styles";
-import {
-  ResponsiveContainer,
-  ComposedChart,
-  AreaChart,
-  LineChart,
-  Line,
-  Area,
-  PieChart,
-  Pie,
-  Cell,
-  YAxis,
-  XAxis,
-} from "recharts";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-// styles
-import useStyles from "./styles";
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
 
-// components
+export default function ControlledExpansionPanels() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
-import Widget from "../../components/Widget";
-import PageTitle from "../../components/PageTitle";
-
-
-
-export default function Dashboard(props) {
-
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
-    <>
-      <PageTitle title="Inicio" />
-      
-      <Grid container spacing={10} >
-        <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Button>
-            <Widget
-              title="Registrar Administradores"
-              disableWidgetMenu
-            >
-            </Widget>
-          </Button>
-        </Grid>
-
-        <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Button>
-            <Widget
-              title="Registrar Docentes"
-              disableWidgetMenu
-            >
-            </Widget>
-          </Button>
-        </Grid>
-
-        <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Button>
-            <Widget
-              title="Registrar Estudiantes"
-              disableWidgetMenu
-            >
-            </Widget>
-          </Button>
-        </Grid>
-
-        <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Button>
-            <Widget
-              title="Registrar Asignaturas"
-              disableWidgetMenu
-            >
-            </Widget>
-          </Button>
-        </Grid>
-
-        <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Button>
-            <Widget
-              title="Registrar Gradosss"
-              disableWidgetMenu
-            >
-            </Widget>
-          </Button>
-        </Grid>
-
-        <Grid item lg={3} md={4} sm={6} xs={12}>
-          <Button>
-            <Widget
-              title="Registrar   Gruposss"
-              disableWidgetMenu
-            >
-            </Widget>
-          </Button>
-        </Grid>
-
-       
-      </Grid>
-
-      
-    </>
+    <div className={classes.root}>
+      <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>General settings</Typography>
+          <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+            maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography className={classes.heading}>Users</Typography>
+          <Typography className={classes.secondaryHeading}>
+            You are currently not an owner
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+            diam eros in elit. Pellentesque convallis laoreet laoreet.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography className={classes.heading}>Advanced settings</Typography>
+          <Typography className={classes.secondaryHeading}>
+            Filtering has been entirely disabled for whole web server
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+            vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography className={classes.heading}>Personal data</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+            vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
   );
 }
