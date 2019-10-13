38fcc79390c111f03b00fd7ce5942e1a084ec7e7
Jaten <33936505+JavierTen@users.noreply.github.com>
2019-10-12 19:24:32 -0500

    Login
    se hizo el frontend del login

---
+++ HelperCloud/src/pages/login/Login.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  CircularProgress,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  TextField,
+  Fade,
+} from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import classnames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// logo
+import logo from "./logo.png";
+import qr from "../../images/qr.png";
+
+// context
+import { useUserDispatch, loginUser } from "../../context/UserContext";
+
+function Login(props) {
+  var classes = useStyles();
+
+  // global
+  var userDispatch = useUserDispatch();
+
+  // local
+  var [isLoading, setIsLoading] = useState(false);
+  var [error, setError] = useState(null);
+  var [activeTabId, setActiveTabId] = useState(0);
+  var [nameValue, setNameValue] = useState("");
+  var [loginValue, setLoginValue] = useState("");
+  var [passwordValue, setPasswordValue] = useState("");
+
+  return (
+    <Grid container className={classes.container}>
+      <div className={classes.logotypeContainer}>
+        <img src={logo} alt="logo" className={classes.logotypeImage} />
+        <Typography  variant="h1" className={classes.logotypeText}>HelperCloud</Typography>
+      </div>
+      <div className={classes.formContainer}>
+        <div className={classes.form}>
+          <Tabs
+            value={activeTabId}
+            onChange={(e, id) => setActiveTabId(id)}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="Iniciar sesión" classes={{ root: classes.tab }} />
+            
+          </Tabs>
+          {activeTabId === 0 && (
+            <React.Fragment>
+              <Typography variant="h1" className={classes.greeting}>
+                Bienvenido.
+              </Typography>
+              <Button size="large" className={classes.googleButton}>
+                <img src={qr} alt="qr" className={classes.googleIcon} />
+                &nbsp;Iniciar con codigo QR.
+              </Button>
+              <div className={classes.formDividerContainer}>
+                <div className={classes.formDivider} />
+                <Typography className={classes.formDividerWord}>o</Typography>
+                <div className={classes.formDivider} />
+              </div>
+              <Fade in={error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
+                </Typography>
+              </Fade>
+              <TextField
+                id="suaurio"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={loginValue}
+                onChange={e => setLoginValue(e.target.value)}
+                margin="normal"
+                placeholder="Usuario"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                margin="normal"
+                placeholder="Contraseña"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.formButtons}>
+                {isLoading ? (
+                  <CircularProgress size={26} className={classes.loginLoader} />
+                ) : (
+                  <Button className={classes.IniciarButton}
+                    disabled={
+                      loginValue.length === 0 || passwordValue.length === 0
+                    }
+                    onClick={() =>
+                      loginUser(
+                        userDispatch,
+                        loginValue,
+                        passwordValue,
+                        props.history,
+                        setIsLoading,
+                        setError,
+                      )
+                    }
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                  >
+                    Iniciar
+                  </Button>
+                )}
+                
+              </div>
+            </React.Fragment>
+          )}
+        </div>       
+      </div>
+    </Grid>
+  );
+}
+
+export default withRouter(Login);
